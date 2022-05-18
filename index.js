6cbfa74297f5b8b6c7afee5368768cc3baacc130
Aditya Ambre <adityaambre17feb@gmail.com>
2022-05-18 21:30:45 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const form = document.getElementById("search");
+const search = document.getElementById("query");
+const stylesApi =
+  "https://iconfinder-api-auth.herokuapp.com/v4/styles?count=30";
+const categoriesApi =
+  "https://iconfinder-api-auth.herokuapp.com/v4/categories?count=30";
+// const iconApi = (query) =>
+//   "https://iconfinder-api-auth.herokuapp.com/v4/icons/search?query=" +
+//   query +
+//   "&count=30";
+let iconApi;
+function icon(query) {
+  iconApi = `https://iconfinder-api-auth.herokuapp.com/v4/icons/search?query=${query}&count=30`;
+}
+
 const options = {
   method: "GET",
   headers: {
@@ -6,13 +21,6 @@ const options = {
       "Bearer ROqwerCTavoSaqEE6G4YDxlovyop5PqLfrPUFfYaEjIr9cBF45fxxr1rVLSnnFZ8",
   },
 };
-
-const stylesApi =
-  "https://iconfinder-api-auth.herokuapp.com/v4/styles?count=30";
-const categoriesApi =
-  "https://iconfinder-api-auth.herokuapp.com/v4/categories?count=30";
-const iconApi =
-  "https://iconfinder-api-auth.herokuapp.com/v4/icons/search?query=Car&count=30";
 
 // Defining async function
 async function getapi(url, type) {
@@ -54,12 +62,11 @@ async function geticon(url) {
   const response = await fetch(url, options);
   // Storing data in form of JSON
   var data = await response.json();
-  console.log(data);
-  // console.log(data.icons[0].raster_sizes[5].formats[0].preview_url);
   let img = ``;
+  console.log(data);
+
   for (let i = 0; i < 30; i++) {
     var url = data.icons[i].raster_sizes[5].formats[0].preview_url;
-    console.log(url);
     img =
       img +
       `<img src="${url}" alt="" width="64" data-icon-id="${data.icons[i].icon_id}">`;
@@ -71,4 +78,15 @@ async function geticon(url) {
 // Calling that async function
 getapi(stylesApi, 0);
 getapi(categoriesApi, 1);
-geticon(iconApi);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const query = search.value;
+
+  if (query) {
+    icon(query);
+    console.log(iconApi);
+    geticon(iconApi);
+  }
+});
